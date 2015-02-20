5f956b342ba94a5bd094d0fd2cd33aaf0ba93d2b
tbepler <tbepler@gmail.com>
2015-02-20 03:46:09 -0500

    changing motif to be CRTP pattern. need to remove string instantiation - can be dealt with at a later time. need to consider whether motif polymorphism is better to be static or runtime.

--- include/bepler/genomics/motif.h
+++ include/bepler/genomics/motif.h
@@ -5,68 +5,110 @@ uifndef INCLUDED_BEPLER_GENOMICS_MOTIF_H
 #include "bepler/functional/continuation.h"
 #include <cstdlib>
 #include <string>
+#include <unordered_map>
 
 namespace genomics{
 
-    template< typename Impl >
-    struct Motif : public Impl{
+    struct Motif{
+        
+        virtual ~Motif() { };
+        virtual double score( const char* str ) const = 0;
+        inline double score( const std::string& str ) const{
+            return score( str.c_str() );
+        }
+        virtual void scoreAll( const char* begin, const char* end, functional::acceptor_f< double >&& out ) const = 0;
+            inline void scoreAll( const std::string& str, functional::acceptor_f< double >&& out ) const{
+                scoreAll( str.c_str(), str.c_str() + str.size(),
+                    std::forward< functional::acceptor_f<double> >( out ) );
+            }
+            inline std::size_t size( ) const { return size_; }
 
-        using Impl::Impl;
+    }; //class Motif
 
-        template< typename G >
-        inline auto score( G&& g ) const{
-            return Impl::score( std::forward<G>( g ) );
-        }
-
-        template< typename G, typename K >
-        inline auto scoreAll( G&& g, K&& k ) const{
-            return Impl::scoreAll( std::forward<G>( g ), std::forward<K>( k ) );
-        }
-
-        template< typename G >
-        inline auto operator( G&& g ) const{
-            return score( std::forward<G>( g ) );
-        }
-
-        template< typename G, typename K >
-        inline auto operator( G&& g, K&& k ) const{
-            return scoreAll( std::forward<G>( g ), std::forward<K>( k ) );
-        }
-
-        inline std::size_t length() const{
-            return Impl::length();
-        }
-
-        template< typename T >
-        inline auto operator==( const T& rhs ) const
-            -> decltype( Impl::equals( rhs ) )
-        {
-            return Impl::equals( rhs );
-        }
-        template< typename T >
-        friend inline bool operator==( const T& lhs, const Motif& rhs ){
-            return rhs.operator==( lhs );
-        }
-        template< typename T >
-        inline auto operator!=( const T& rhs ) const
-            -> decltype( !Impl::equals( rhs ) )
-        {
-            return !Impl::equals( rhs );
-        }
-        template< typename T >
-        friend inline bool operator!=( const T& lhs, const Motif& rhs ){
-            return rhs.operator!=( lhs );
-        }
-
-        friend inline std::istream& operator>>( std::istream& in, Motif& m ){
-            m.read( in ); return in;
-        }
-
-        friend inline std::ostream& operator<<( std::ostream& out, const Motif& m ){
-            m.write( out ); return out;
-        }
-
-    };
+    namespace motif{
+    
+        namespace detail{
+    
+            template< typename Impl >
+            struct Motif : public Impl{
+        
+                using Impl::Impl;
+        
+                template< typename G >
+                inline auto score( G&& g ) const{
+                    return Impl::score( std::forward<G>( g ) );
+                }
+        
+                template< typename G, typename K >
+                inline auto scoreAll( G&& g, K&& k ) const{
+                    return Impl::scoreAll( std::forward<G>( g ), std::forward<K>( k ) );
+                }
+        
+                template< typename G >
+                inline auto operator( G&& g ) const{
+                    return score( std::forward<G>( g ) );
+                }
+        
+                template< typename G, typename K >
+                inline auto operator( G&& g, K&& k ) const{
+                    return scoreAll( std::forward<G>( g ), std::forward<K>( k ) );
+                }
+        
+                inline std::size_t length() const{
+                    return Impl::length();
+                }
+        
+                template< typename T >
+                inline auto operator==( const T& rhs ) const
+                    -> decltype( Impl::equals( rhs ) )
+                {
+                    return Impl::equals( rhs );
+                }
+                template< typename T >
+                friend inline bool operator==( const T& lhs, const Motif& rhs ){
+                    return rhs.operator==( lhs );
+                }
+                template< typename T >
+                inline auto operator!=( const T& rhs ) const
+                    -> decltype( !Impl::equals( rhs ) )
+                {
+                    return !Impl::equals( rhs );
+                }
+                template< typename T >
+                friend inline bool operator!=( const T& lhs, const Motif& rhs ){
+                    return rhs.operator!=( lhs );
+                }
+        
+                friend inline std::istream& operator>>( std::istream& in, Motif& m ){
+                    m.read( in ); return in;
+                }
+        
+                friend inline std::ostream& operator<<( std::ostream& out, const Motif& m ){
+                    m.write( out ); return out;
+                }
+        
+            };
+    
+            enum Type{
+                PWM,
+                FeatureModel,
+                PKKModel
+            };
+    
+            const std::unordered_map< std::string, Type > makeRegistry(){
+                std::unordered_map< std::string, Type > r;
+                r[ "pwm" ] = PWM;
+                r[ "feature-model" ] = FeatureModel;
+                r[ "pkk-model" ] = PKKModel;
+                return r;
+            }
+    
+            const std::unordered_map< std::string, Type > registry
+                = makeRegistry();
+    
+        } //namespace detail
+    
+    } //namespace motif
 
     auto motifType( const std::string& tag ){
 
@@ -76,6 +118,7 @@ namespace genomics{
     void parseMotif( const std::string& tag, std::istream& in, K&& k, Params&&... args ){
         
         switch( motifType( tag ) ){
+
             /*
             case Type:
                 Motif<Type> m;
@@ -85,29 +128,6 @@ namespace genomics{
         }
 
     }
-
-    class Motif{
-        
-        public:
-            virtual ~Motif() { };
-            virtual double score( const char* str ) const = 0;
-            inline double score( const std::string& str ) const{
-                return score( str.c_str() );
-            }
-            virtual void scoreAll( const char* begin, const char* end, functional::acceptor_f< double >&& out ) const = 0;
-            inline void scoreAll( const std::string& str, functional::acceptor_f< double >&& out ) const{
-                scoreAll( str.c_str(), str.c_str() + str.size(),
-                    std::forward< functional::acceptor_f<double> >( out ) );
-            }
-            inline std::size_t size( ) const { return size_; }
-
-        protected:
-            std::size_t size_;
-            Motif() : size_( 0 ) { }
-            Motif( std::size_t size ) : size_( size ) { }
-            Motif( const Motif& rhs ) : size_( rhs.size_ ) { }
-
-    }; //class Motif
 
 } //namespace genomics
 
